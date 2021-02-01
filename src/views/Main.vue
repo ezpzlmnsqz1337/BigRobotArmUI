f347f60609213344bd44e8fc4c63a8b75352b0e9
ezpzlmnsqz1337 <ezpzlmnsqz1337@github.com>
2021-02-01 19:19:04 +0100

    Working commands and connection

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,12 +1,26 @@
 <template>
   <b-container fluid class="text-center my-auto" style="height: 100%">
     <h1>Big Robot Arm UI</h1>
-    <b-row>
+    <b-row v-show="!isConnected">
+      <b-col>
+        <b-button variant="success" size="large" @click="connect()"
+          >Connect</b-button
+        >
+      </b-col>
+    </b-row>
+    <b-row v-show="isConnected">
       <b-col>
         <Terminal />
       </b-col>
       <b-col>
         <ManualControl />
+      </b-col>
+    </b-row>
+    <b-row v-show="isConnected">
+      <b-col>
+        <b-button variant="danger" size="large" @click="disconnect()"
+          >Disconnect</b-button
+        >
       </b-col>
     </b-row>
   </b-container>
@@ -24,16 +38,35 @@ export default {
     Terminal
   },
   data() {
-    return {}
+    return {
+      connectionStatus: 0
+    }
+  },
+  computed: {
+    isConnected: function() {
+      return this.connectionStatus == 1
+    }
   },
   created() {
     ws.onopen = () => {
-      ws.send('getPositions')
+      // ws.send('getPositions')
     }
 
     ws.onmessage = event => {
       console.log('Response from server: ', event.data)
+
+      if (event.data.includes('connectionStatus')) {
+        this.connectionStatus = parseInt(event.data.split(':')[1])
+      }
       this.$root.$emit('ws-message-received', event.data)
+    }
+  },
+  methods: {
+    connect: function() {
+      ws.send('connect')
+    },
+    disconnect: function() {
+      ws.send('disconnect')
     }
   }
 }
