c64d124d9506bf7f5a12b98069afd80d0aed474c
ezpzlmnsqz1337 <ezpzlmnsqz1337@github.com>
2021-03-06 02:19:06 +0100

    Added editation of sequence

--- src/components/Sequences.vue
+++ src/components/Sequences.vue
@@ -15,7 +15,7 @@
         step="1"
       />
     </b-form-group>
-    <b-list-group>
+    <b-list-group v-if="!edit">
       <b-list-group-item
         v-for="(s, index) in sequences"
         :key="`seq-${index}`"
@@ -29,14 +29,37 @@
             <h5 class="mb-1">{{ s.name }}</h5>
             <small>{{ s.data.length }} commands</small>
           </div>
-          <b-button variant="danger" size="sm" v-b-modal.remove-sequence-modal
-            >&#x1F5D1; Remove</b-button
-          >
+          <b-button-group>
+            <b-button size="sm" @click="editSequence(index)"
+              >&#9998; Edit</b-button
+            >
+            <b-button variant="danger" size="sm" v-b-modal.remove-sequence-modal
+              >&#x1F5D1; Remove</b-button
+            >
+          </b-button-group>
         </div>
       </b-list-group-item>
+      <b-button @click="play()" :disabled="disabled">Play</b-button>
+      <b-button @click="preview()">Preview</b-button>
     </b-list-group>
-    <b-button @click="play()" :disabled="disabled">Play</b-button>
-    <b-button @click="preview()">Preview</b-button>
+    <!-- Edit sequence -->
+    <b-container v-if="edit && editedSequence" class="__edit">
+      <h2>Edit sequence</h2>
+      <label for="sequenceName">Name</label>
+      <b-form-input if="sequenceName" v-model="editedSequence.name" />
+      <label for="sequenceData" class="mt-3">Data</label>
+      <b-textarea
+        if="sequenceData"
+        v-model="editedSequence.data"
+        :rows="editedSequenceRows"
+      />
+      <b-button variant="success" @click="saveSequence()"
+        >&#x1F5AB; Save</b-button
+      >
+      <b-button variant="danger" @click="cancelEditSequence()"
+        >&#x1F5D1; Cancel</b-button
+      >
+    </b-container>
     <!-- modal -->
     <b-modal
       id="remove-sequence-modal"
@@ -76,7 +99,9 @@ export default {
       joints: this.$arm.joints,
       gripper: this.$arm.gripper,
       previewQueue: [],
-      previewSpeed: 5
+      previewSpeed: 5,
+      edit: false,
+      editedSequence: null
     }
   },
   created: function() {
@@ -84,6 +109,11 @@ export default {
     eb.on(EventType.ARM_IN_POSITION, () =>
       this.previewCommand(this.previewQueue.shift())
     )
+  },
+  computed: {
+    editedSequenceRows: function() {
+      return this.editedSequence.data.split('\n').length
+    }
   },
   methods: {
     play() {
@@ -117,6 +147,27 @@ export default {
     removeSequence(index) {
       this.$store.removeSequence(index)
       this.$bvModal.hide('remove-sequence-modal')
+    },
+    editSequence(index) {
+      this.selected = index
+      this.editedSequence = {
+        name: this.sequences[index].name,
+        data: this.sequences[index].data.join('\n')
+      }
+      this.edit = true
+    },
+    saveSequence() {
+      this.sequences[this.selected].name = this.editedSequence.name
+      this.sequences[this.selected].data.splice(0)
+      this.sequences[this.selected].data.push(
+        ...this.editedSequence.data.split('\n')
+      )
+      this.edit = false
+      this.$store.saveSequences()
+    },
+    cancelEditSequence() {
+      this.edit = false
+      this.editedSequence = null
     }
   }
 }
@@ -126,5 +177,8 @@ export default {
 <style scoped lang="scss">
 .__sequence {
   height: 15rem;
+}
+.__edit {
+  text-align: left;
 }
 </style>
